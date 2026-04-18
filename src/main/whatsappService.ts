27925acf7db05810d61b727ee5d4c93c8600518d
schecko0 <caballerobustossergio@gmail.com>
2026-04-17 20:15:38 -0600

    se carga whatsapp mas ligero

--- src/main/whatsappService.ts
+++ src/main/whatsappService.ts
@@ -529,7 +529,17 @@ export function initWhatsAppClient(): Promise<void> {
           '--disable-accelerated-2d-canvas',
           '--no-first-run',
           '--disable-gpu',
+          '--disable-extensions',
+          '--disable-background-networking',
+          '--disable-default-apps',
+          '--disable-sync',
+          '--no-zygote',
+          '--single-process',
         ],
+      },
+      webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
       },
     })
 
